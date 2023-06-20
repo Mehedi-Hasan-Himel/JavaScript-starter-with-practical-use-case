5852a5eff5c6ed5cc85bb93bfc567ff18ca4e7eb
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 04:39:28 +0600

    js math objects

---
+++ 2-JavaScript-Math-Object.js
@@ -0,0 +1,16 @@
+// Math Object
+// var num = -7;
+// var num = 7.2;
+// var num = 7.9;
+var num = 7.5;
+
+document.write(
+   // Math.abs(num) 
+   // Math.ceil(num)
+   // Math.floor(num)
+   // Math.max(22,33,444,777,55)
+   // Math.min(22,33,444,777,55,4)
+   // Math.random()
+   Math.round(num)
+
+);
