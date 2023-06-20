1466baeda7d39725de17615d8b58499f49c4bd37
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 04:47:32 +0600

    js numbers

---
+++ 3-JavaScript-Numbers.js
@@ -0,0 +1,11 @@
+// 3. JavaScript Numbers
+var num = 3.88896675;
+
+document.write(
+  // Number.isFinite(num)
+  // Number.isInteger(num)
+  // Number.parseFloat(num)
+  // Number.parseInt(num)
+  //   num.toFixed(2)
+  num.toString()
+);
