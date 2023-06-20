e473d16e6fbce62cf5210f85b3d7306dbbb27186
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 05:16:49 +0600

    5 JS Navigator Object

---
+++ 5-JS-Navigator-Object.js
@@ -0,0 +1,12 @@
+// JavaScript Navigator Object
+
+var appCodeName = navigator.appCodeName;
+var appName = navigator.appName;
+
+var appVersion = navigator.appVersion;
+var language = navigator.language;
+var userAgent = navigator.userAgent;
+var platform = navigator.platform;
+
+document.write(userAgent + "<br>");
+
