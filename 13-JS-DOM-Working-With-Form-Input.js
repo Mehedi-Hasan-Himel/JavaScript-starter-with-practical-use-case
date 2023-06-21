c41b302f2062f35df83c1101181aff6310604460
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 13:27:23 +0600

    14 DOM Manupulate CSS Class

---
+++ 13-JS-DOM-Working-With-Form-Input.js
@@ -0,0 +1,9 @@
+function AddTwoNumber() {
+  let num1 = document.getElementById("num1").value;
+   let num2 = document.getElementById("num2").value;
+   
+
+   let result = parseFloat(num1) + parseFloat(num2);
+
+   alert(result);
+}
---
+++ 14-DOM-Manupulate-CSS-Class.js
@@ -0,0 +1,8 @@
+function AddStyle() {
+  let H1Tag = document.getElementById("MyID");
+  H1Tag.classList.add("text-primary");
+}
+function RemoveStyle() {
+  let H1Tag = document.getElementById("MyID");
+  H1Tag.classList.remove("text-primary");
+}
