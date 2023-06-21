abc2ab23c7eb17ecbe0a515dc1f9be68f525c749
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 11:14:31 +0600

    10 Finding Elements By DOM

---
+++ 10-Finding-Elements-By-DOM.js
@@ -0,0 +1,8 @@
+// var MyElement = document.getElementById("MyId");
+
+// MyElement.innerHTML = "ok bye !";
+
+var MyElements = document.getElementsByClassName("MyClass");
+
+MyElements[2].innerHTML = "see you not for mind !";
+
