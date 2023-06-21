a3ecd28f08b481892397412c20e3544545f09610
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-21 13:50:27 +0600

    16 DOM Change Attribute Value

---
+++ 16-DOM-Change-Attribute-Value.js
@@ -0,0 +1,7 @@
+function ChangeAttribute() {
+   let myImg = document.getElementById("MyImg");
+   myImg.src =
+     "https://media.licdn.com/dms/image/C5603AQGqHcx958Kidw/profile-displayphoto-shrink_800_800/0/1638375438078?e=1692835200&v=beta&t=NPh7RKNzs-Cy7qAktPymBcxbU2rEGiJ0A99SsTpVkr8";
+   myImg.height = "400"
+   myImg.width = "400"
+}
